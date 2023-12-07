6213869c38136c009e5fef650c2e49131bbed1d7
srknssmn <srknssmn@gmail.com>
2023-12-07 04:08:19 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
       // To exclude specific polyfills, add them to this list.
       exclude: [
         'fs', // Excludes the polyfill for `fs` and `node:fs`.
-        'http'
+        'http',
+        'node_modules/@quais/contracts/lib.esm/index.js'
       ],
       // Whether to polyfill specific globals.
       globals: {
