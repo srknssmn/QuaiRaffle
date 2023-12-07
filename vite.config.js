3c5788dd490715251a61ed41736f6b71c93aebc3
srknssmn <srknssmn@gmail.com>
2023-12-07 04:44:38 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,32 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'node:path';
 
-const pathAd = path.resolve(__dirname, 'node_modules/@quais/contracts/lib.esm/index.js')
+const pathAd = path.resolve('node_modules/@quais/contracts/lib.esm/index.js')
+console.log(pathAd)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    exclude: [
-      pathAd
-    ]
-  })],
+  plugins: [
+    react({
+      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
+      include: ['path'],
+      // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
+      exclude: [
+        pathAd
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Override the default polyfills for specific modules.
+      overrides: {
+        // Since `fs` is not supported in browsers, we can use the `memfs` package to polyfill it.
+        fs: 'memfs',
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
 })
